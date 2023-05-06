90d3f05538d745e8d11d7b1a70ff634dcfe218d7
Shahrukh <Shahrukh@MacBook-Pro-2.local>
2023-05-07 00:02:00 +0530

    adding finally

--- src/Notes/PromiseNotes.js
+++ src/Notes/PromiseNotes.js
@@ -6,4 +6,6 @@ promise.then(function success(data){
     console.log(data)
 }).catch(function failed(error){
    console.log(error)
-},)
+}).finally(function final(){
+    console.log("Go away")
+})
